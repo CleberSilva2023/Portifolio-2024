1279310d90cbc6b0378a6198d1436b16dd1271df
CleberSilva <clebercapricornio@gmail.com>
2024-05-28 20:02:43 -0300

    formulario

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -14,3 +14,36 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+//FORMULÁRIO
+const form = document.getElementById("formulario");
+const nome = document.getElementById("nome");
+const email = document.getElementById("e-mail");
+const celular = document.getElementById("celular");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    checkInputUsername();
+});
+
+function checkInputUsername(){
+    const nomev = nome.value;
+
+    if(namev === "" ){
+        errorInput(nome, "Preencha o NOME!")
+    }else{
+        const formItem = nome.parentElement;
+        formItem.classList = "form-content"
+    }
+}
+
+function errorInput(input,message){
+    const formItem = input.pareElement;
+    const textMessage = formItem.querySelector("#erro")
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error"
+}
